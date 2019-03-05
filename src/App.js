3852801559fd9c8467b942ecaa6c1ed7f196dc74
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-03-05 23:32:21 +0200

    create main navbar class-based component. add the outside links for bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Components
+import AppNavbar from "./components/layout/AppNavbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Start</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+        </div>
+      </Router>
     );
   }
 }
